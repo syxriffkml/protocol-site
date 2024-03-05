b5d3adbb503d316ce1c9b0e4123d767d6a1b399b
Nazrin Amirrullah bin Zaidi <147366697+nazrinamir@users.noreply.github.com>
2024-03-05 19:29:05 +0800

    edited poistion UI

--- src/routes/position/+page.svelte
+++ src/routes/position/+page.svelte
@@ -1,3 +1,21 @@
-<div>
-    Get Rekt 2.0
+<script lang="ts">
+    import Section from "$lib/Components/Box/Box.svelte"
+    import Icon from "@iconify/svelte";
+    import Button from "$lib/Components/Button/Button.svelte";
+    
+</script>
+
+<div class="h-[200vh] flex flex-col xl:max-w-[90rem] items-start mx-auto w-full p-6 gap-y-4">
+    <div class=" w-full">
+        <Section customClass={"w-[80%] mx-auto xl:mx-0 xl:max-w-[30%]"}>
+            <div>
+                <div>
+                    +
+                </div>
+                <div>
+                    Create Position
+                </div>
+            </div>
+        </Section>
+    </div>
 </div>
