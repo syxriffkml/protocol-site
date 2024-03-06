89e8f22585dbfd0a3d4bfd06ea06fa86940bac3f
Strawberry Cheesecake <55937579+syxriffkml@users.noreply.github.com>
2024-03-06 11:45:22 +0800

    added BoxImage

---
+++ src/lib/Components/Box/BoxImage.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+
+</script>
+
+<div class="banner bg-[#232f33] rounded-lg w-full h-[272px] p-10 mt-10">
+    <slot/>
+</div>
+
+<style>
+    /* .banner {
+        @apply relative bg-cover bg-center;
+        background-image: url('https://app.bucketprotocol.io/_next/image?url=%2Fimages%2F3d-sui-bg.png&w=750&q=75');
+    }
+
+    .banner::before {
+        content: "";
+        @apply absolute inset-0 bg-[#232f33] opacity-0;
+        pointer-events: none;
+    } */
+
+
+    .banner {
+        @apply relative bg-cover bg-center rounded-lg z-[1];
+        background: conic-gradient(from 202deg at 50% 50%, #202f34 42.57809400558472deg, #65e0fc 94.842449426651deg, #a7edfd 106.2618899345398deg, #65e0fc 119.4994604587555deg, #202f34 187.49999284744263deg, #202f34 260.7220673561096deg, #9beeff 286.23008966445923deg, #202f34 308.08024406433105deg);
+    }
+
+    .banner::before {
+        content: "";
+        @apply absolute inset-px bg-[#232f33] rounded-lg z-[-1];
+        background-image: url('https://app.bucketprotocol.io/_next/image?url=%2Fimages%2F3d-sui-bg.png&w=750&q=75');
+        background-size: cover ;
+        pointer-events: none;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,58 +1,84 @@
 <script lang="ts">
 	import Button from "$lib/Components/Button/Button.svelte";
 	import Icon from '@iconify/svelte';
-
+	import CardImg from "$lib/Components/Box/BoxImage.svelte";
+	import Section from "$lib/Components/Box/Box.svelte";
 </script>
 
-<div class="h-full w-full flex flex-col">
+<div class="h-screen w-full flex flex-col xl:max-w-[90rem] items-start mx-auto p-6">
 
-	<div class="flex flex-row w-full">
-		<div class="flex flex-col w-full">
-			<div class="">
-				1
-			</div>
-			<div class="flex flex-row">
-				<div class="">
-					2
+	<div class="flex flex-row w-full gap-x-6">
+		<div class="flex flex-col w-full gap-y-6">
+				
+			<CardImg>
+				<div class="flex flex-col w-full h-full gap-y-8">
+					<p class="text-3xl font-semibold">Earn up to 67.37%<br>APR of $SUI</p>
+					<Button width="w-[200px]" mode="blue" customClass="flex items-center justify-center gap-x-2 hover:gap-x-6 !transition-all !duration-200">
+						<p class="text-xl">Explore</p>
+						<div class="bg-black rounded-full w-auto p-1">
+							<Icon icon="mingcute:arrow-right-line" class="w-5 h-5 text-white"/>
+						</div>
+					</Button>
 				</div>
-				<div class="flex flex-col">
-					<div class="">
-						3
+			</CardImg>
+
+			<div class="flex flex-row w-full gap-x-4">
+
+
+				<div class="w-[50%]">
+					<Section customClass="!p-8">
+						1
+					</Section>
+				</div>
+
+
+				<div class="flex flex-col w-[50%] gap-y-4">
+					<div class="w-full">
+						<Section customClass="!p-8">
+							2
+						</Section>
 					</div>
 					<div class="">
-						4
+						<Section customClass="!p-8">
+							3
+						</Section>
 					</div>
 				</div>
+
+
 			</div>
 		</div>
-		<div class="flex flex-col w-full">
+
+
+
+		<div class="flex flex-col w-full gap-y-6">
+
+			<CardImg>
+				<div class="flex flex-col w-full h-full gap-y-8">
+					<p class="text-3xl font-semibold">Use BUCK<br>in Suilette!</p>
+					<Button width="w-[200px]" mode="blue" customClass="flex items-center justify-center gap-x-2 hover:gap-x-6 !transition-all !duration-200">
+						<p class="text-xl">Try Out</p>
+						<div class="bg-black rounded-full w-auto p-1">
+							<Icon icon="mingcute:arrow-right-line" class="w-5 h-5 text-white"/>
+						</div>
+					</Button>
+				</div>
+			</CardImg>
+
 			<div class="">
-				1
+				<Section customClass="!p-8">
+					4
+				</Section>
 			</div>
-			<div class="">
-				2
-			</div>
+
+
+
 		</div>
 	</div>
 
-	
-	<!-- <Button width="w-[200px]" mode="blue" customClass="flex items-center justify-center gap-x-2 hover:gap-x-6 !transition-all !duration-200">
-		<p class="text-xl">Explore</p>
-		<div class="bg-black rounded-full w-auto p-1">
-			<Icon icon="mingcute:arrow-right-line" class="w-5 h-5 text-white"/>
-		</div>
-	</Button>
 
-	<Button width="w-[200px]" mode="blue" customClass="flex items-center justify-center gap-x-2 hover:gap-x-6 !transition-all !duration-200">
-		<p class="text-xl">Explore</p>
-		<div class="bg-black rounded-full w-auto p-1">
-			<Icon icon="mingcute:arrow-right-line" class="w-5 h-5 text-white"/>
-		</div>
-	</Button> -->
-
-
-
-	
 
 
 </div>
+
+
