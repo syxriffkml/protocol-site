2d69828644f28c9e46a91246b413e6b047c8b27d
Nazrin Amirrullah bin Zaidi <147366697+nazrinamir@users.noreply.github.com>
2024-03-06 14:49:18 +0800

    adjusted

--- src/lib/Components/Box/BoxOverFlow.svelte
+++ src/lib/Components/Box/BoxOverFlow.svelte
@@ -10,21 +10,19 @@
 
 </script>
 
-<div class="flex ">
-    <div class="hidden xl:block xl:w-[54px]" />
-    <div class="w-full">
-        <Section customClass={"relative min-w-[40%] space-y-4 pt-8 px-8 pb-6"} {animate}>
-            <div class="pt-4">
-                <div class="xl:-translate-x-[65px] -translate-y-[30px] absolute bg-white z-50  rounded-full p-1.5">
-                    <img src="https://app.bucketprotocol.io/images/vsui-icon.svg" alt="">
-                </div>
-                <div class="xl:-translate-x-[40px] -translate-y-[15px] w-fit xl:w-[200px] bg-white text-black text-2xl text-right p-1 pr-4 font-semibold rounded-full">
-                    <div class="ml-20">
-                        {header}
-                    </div>
+<div class="flex w-auto">
+    <div class="hidden xl:block xl:min-w-[54px]" />
+    <Section customClass={"relative w-full space-y-4 pt-8 px-8 pb-6"} {animate}>
+        <div class="pt-4">
+            <div class="xl:-translate-x-[65px] -translate-y-[30px] absolute bg-white z-50  rounded-full p-1.5">
+                <img src="https://app.bucketprotocol.io/images/vsui-icon.svg" alt="">
+            </div>
+            <div class="xl:-translate-x-[40px] -translate-y-[15px] w-fit xl:w-[200px] bg-white text-black text-2xl text-right p-1 pr-4 font-semibold rounded-full">
+                <div class="ml-20">
+                    {header}
                 </div>
             </div>
-            <slot></slot>
-        </Section>
-    </div>
+        </div>
+        <slot></slot>
+    </Section>
 </div>
