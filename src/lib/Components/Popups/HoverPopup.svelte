698b56441584763985718c4b7336ba8c1326434d
Strawberry Cheesecake <55937579+syxriffkml@users.noreply.github.com>
2024-03-07 14:04:37 +0800

    change hoverpopup Button into Div

--- src/lib/Components/Popups/HoverPopup.svelte
+++ src/lib/Components/Popups/HoverPopup.svelte
@@ -2,44 +2,51 @@
 <script lang="ts">
 	import { fade } from "svelte/transition";
 
-    export let content = '';
-    export let header = '';
+  export let content = '';
+  export let header = '';
+
+  let isVisible = false;
+
+  function handleMouseEnter() {
+    isVisible = true;
+  }
+
+  function handleMouseLeave() {
+    isVisible = false;
+  }
+</script>
   
-    let isVisible = false;
-  
-    function handleMouseEnter() {
-      isVisible = true;
-    }
-  
-    function handleMouseLeave() {
-      isVisible = false;
-    }
-  </script>
-  
-  <button class="relative"  on:mouseenter={handleMouseEnter} on:mouseleave={handleMouseLeave} >
-    <slot />
-  
-    {#if isVisible}
-      <div class="fixed z-10" >
-        <div class="border border-gray-700 w-[10rem] xl:w-[20rem] bg-[#141b1d] rounded-lg p-2 absolute top-0 left-1/2 transform -translate-x-1/2" in:fade out:fade>
-          <div class="text-sm font-semibold text-white">
-            {header}
-          </div>
-          <div class="text-xs text-slate-400 ">
-            {content}
-          </div>
+<div class="relative" 
+  on:mouseenter={handleMouseEnter} 
+  on:mouseleave={handleMouseLeave} 
+  tabindex="0" 
+  role="button" 
+  aria-label="..."
+>
+
+  <slot />
+
+  {#if isVisible}
+    <div class="fixed z-10" >
+      <div class="border border-gray-700 w-[10rem] xl:w-[20rem] bg-[#141b1d] rounded-lg p-2 absolute top-0 left-1/2 transform -translate-x-1/2" in:fade out:fade>
+        <div class="text-sm font-semibold text-white">
+          {header}
+        </div>
+        <div class="text-xs text-slate-400 ">
+          {content}
         </div>
       </div>
-    {/if}
+    </div>
+  {/if}
+
+</div>
   
-    <style>
-      .relative {
-        position: relative;
-      }
-  
-      .fixed {
-        position: fixed;
-      }
-    </style>
-  </button>
-  
+<style>
+  .relative {
+    position: relative;
+  }
+
+  .fixed {
+    position: fixed;
+  }
+</style>
